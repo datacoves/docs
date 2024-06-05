cb320bb4f298f42a74647de6a7426f8604d9e7ff
Stephen Conley <stephen@datacoves.com>
2024-06-05 15:44:18 -0400

    Added search

--- docs/assets/custom.js
+++ docs/assets/custom.js
@@ -52,6 +52,88 @@ jQuery(document).ready(function() {
             'Datacoves Docs - ' + potential_titles.text()
         );
     }
+
+    // Create a map of URLs to titles
+    var titleMap = {};
+
+    jQuery('div.sidebar-nav').find('a').each(function() {
+        var jqThis = jQuery(this);
+        titleMap[jqThis.attr('href')] = jqThis.text();
+    });
+
+    // Clear search
+    jQuery('#searchbox').val('');
+
+    // Set up search
+    jQuery.ajax(
+        '/assets/search-index.js',
+        {
+            dataType: 'text',
+            method: 'GET',
+            success: function (data) {
+                var index = MiniSearch.loadJSON(data, {fields: ['text']});
+
+                jQuery('#searchbox').on('input', function (ev) {
+                    var results = index.search(
+                        jQuery(this).val(),
+                        {
+                            prefix: true,
+                            fuzzy: 0.3,
+                        }
+                    );
+
+                    // Clear results div
+                    var resultsDiv = jQuery('#resultsbox');
+                    resultsDiv.empty();
+
+                    // Take the first 10 results
+                    for (var i = 0; i < results.length && (i < 10); i++) {
+                        var newDiv = jQuery('<div class="matching-post"></div>');
+                        var newA = jQuery('<a></a>');
+                        var newH2 = jQuery('<h2></h2>');
+                        var newP = jQuery('<p></p>');
+
+                        if (titleMap[results[i].url]) {
+                            newH2.html(titleMap[results[i].url]);
+                        } else {
+                            newH2.text("Datacoves Page");
+                        }
+
+                        // Add a snip
+                        newP.html(
+                            results[i].snip.substr(
+                                results[i].snip.toLowerCase().indexOf(
+                                    results[i].queryTerms[0].toLowerCase()
+                                ),
+                                50
+                            )
+                            +
+                            "..."
+                        );
+
+                        newA.attr('href', results[i].url);
+
+                        newA.append(newH2);
+                        newA.append(newP);
+                        newDiv.append(newA);
+                        resultsDiv.append(newDiv);
+                    }
+
+                    resultsDiv.addClass('show');
+                    jQuery('div.search').addClass('show');
+                    jQuery('button.clear-button').addClass('show');
+                });
+            }
+        }
+    );
+
+    // Handle clear button
+    jQuery('button.clear-button').click(function (ev) {
+        jQuery('#resultsbox').empty().removeClass('show');
+        jQuery('div.search').removeClass('show');
+        jQuery('button.clear-button').removeClass('show');
+        jQuery('#searchbox').val('');;
+    });
 });
 
 // Add edit on github support
